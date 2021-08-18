f108020f5cdd7de7ae86cf71228bcb3604282985
Your Name <you@example.com>
2021-08-18 12:41:33 +0630

    auto

--- examples/test_contains_selector.py
+++
@@ -1,9 +0,0 @@
-from seleniumbase import BaseCase
-
-
-class ContainsSelectorTests(BaseCase):
-    def test_contains_selector(self):
-        self.open("https://xkcd.com/2207/")
-        self.assert_element('div.box div:contains("Math Work")')
-        self.click('a:contains("Next")')
-        self.assert_element('div div:contains("Drone Fishing")')
